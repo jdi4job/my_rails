d34e73ea638a33b2cd1addc312f26db02e521db3
zhouxin <zhouxin@yundianjia.com>
2016-10-24 11:44:29 +0800

    seconde commit

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,10 @@
+class TestController < ApplicationController
+
+  def self.create_user
+    #@user = User.create(name: 'zhou', email: '736502531@qq.com', login: 'xin')
+  end
+
+  def send_mail
+
+  end
+end
